9da910e5e605d5d0a7cb5bf08538c56a49d59f7a
SergioHZ <alejo.80.123@hotmail.com>
2021-05-05 23:13:46 -0500

    Imagenes

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -7,3 +7,36 @@ function doOpen(url) {
     document.location.target = "_blank";
     document.location.href = url;
 }
+
+function printTable(elementId, servlet, columns,actions = []) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            var data = JSON.parse(xhr.responseText);
+            var tbodyRef = document.getElementById(elementId).getElementsByTagName('tbody')[0];
+            data.map(d => {
+                var newRow = tbodyRef.insertRow();
+                columns.map(c => {
+                    var cell = newRow.insertCell();
+                    var text = document.createTextNode(d[c]);
+                    cell.appendChild(text);
+                });
+                if (actions.includes('create-book')) {
+                    var cell = newRow.insertCell();
+                    var action = document.createElement('button');
+                    //  action.setAttribute('onclick', 'location.href="./upload.jsp?file=' + d['file'] +'ocation.href="./upload"'+ d['upload']+';');
+                    var text = document.createTextNode('Descargar');
+                    action.appendChild(text);
+                    cell.appendChild(action);
+                }
+            });
+        }
+    }
+    xhr.open('GET', '${pageContext.request.contextPath}/' + servlet, true);
+    xhr.send(null);
+}
+// Printing authors
+
+printTable(elementId = 'authorsTbl', servlet = 'GetCities', columns = ['nombre', 'fecha', 'descripcion', <img src="C:\\Users\\SergioHZ\\IdeaProjects\\Taller-4\\src\\main\\resources\\images\\"  width="500" height="600"/> ],actions = ['create-book']);
+
+
--- src/main/webapp/photographs.jsp
+++ src/main/webapp/photographs.jsp
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-<form action="accion" class="col-md-6" method="get" enctype="multipart/form-data">
+<form action="accion" class="col-md-6" method="post" enctype="multipart/form-data">
     <div class="card">
         <div class="card-header">
             <h3>Agregar Imagenes</h3>
@@ -24,7 +24,7 @@
             </div>
         </div>
         <div class="card-footer">
-            <input type="submit" class="btn btn-outline-primary" name="btnEnviar" value="Guardar">
+            <input type="submit" class="btn btn-outline-primary" name="btnEnviar" value="Guardar" >
         </div>
     </div>
 </form>
